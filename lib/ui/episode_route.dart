7559d21c71be8c18d338bc15d4fb4f53f53423d1
Martin <591337@stud.hvl.no>
2022-06-18 00:45:24 +0200

    remove unused

--- lib/ui/episode_route.dart
+++ lib/ui/episode_route.dart
@@ -4,35 +4,6 @@ import 'package:flutter/material.dart';
 import '../controller.dart';
 import '../podcast/episode.dart';
 import '../podcast/podcast.dart';
-/*
-class EpisodeRoute {
-  static Widget selectionEpisodes(Controller controller, Podcast p) {
-    return Scaffold(
-        appBar: AppBar(title: Text(p.title)),
-        body: ListView(children: episodeList(controller, p)));
-  }
-
-  static List<Widget> episodeList(Controller controller, Podcast p) {
-    List<Widget> list = [];
-    for (Episode e in p.episodes) {
-      list.add(Card(
-        child: Row(
-          children: [
-            Text(e.title),
-            IconButton(onPressed: () {
-              controller.playEpisode(e);},
-                icon: const Icon(Icons.play_arrow)),
-            IconButton(onPressed: () {
-              controller.downloadEpisode(e);
-            }, icon: const Icon(Icons.file_download))
-          ],
-        ),
-      ));
-    }
-    return list;
-  }
-}
- */
 
 typedef DownloadEpisode = Function(Episode episode);
 typedef PlayEpisode = Function(Episode episode);
