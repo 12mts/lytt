9a9dd932478c5bf735b7b645c3cdea148d09ebdf
Martin <591337@stud.hvl.no>
2022-06-17 23:30:22 +0200

    Image, title and play/pause button changes now

--- lib/main.dart
+++ lib/main.dart
@@ -8,18 +8,3 @@ void main() {
   ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the front page
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Lytt',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const LyttApp(title: "Test"),
-    );
-  }
-}
